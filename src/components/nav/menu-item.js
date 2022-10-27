5c7b142469a4b4ab6f9aee877bceb743b33c0c4c
edwinwalela <edwinwalela@gmail.com>
2022-10-27 16:55:49 +0300

    active menu item

--- src/components/nav/menu-item.js
+++ src/components/nav/menu-item.js
@@ -18,9 +18,9 @@ function MenuItem(props){
   let active
 
   return (
-    <div className="flex py-3 px-6 cursor-pointer group transition-all duration-200 ease-out hover:border-l-4 border-purple-700">
-      <FontAwesomeIcon className="pr-6 text-gray-500 group-hover:text-purple-700" icon={icons[props.title]} size="lg"/>
-      <p className="text-gray-400 font-medium flex-1 hover:scale-95 transition-all duration-200 ease-out group-hover:text-purple-700">{props.title}</p>
+    <div className={`flex py-3 px-6 mb-2 cursor-pointer group transition-all duration-200 ease-out hover:border-l-4 border-purple-700 ${props.active ? "border-l-4 border-purple-700":""}`}>
+      <FontAwesomeIcon className={`pr-6 text-gray-500 group-hover:text-purple-700 ${props.active ? "text-purple-700":""}`} icon={icons[props.title]} size="lg"/>
+      <p className={`text-gray-400 font-medium flex-1 ${!props.active ? "hover:scale-95 ":""} transition-all duration-200 ease-out group-hover:text-purple-700 ${props.active ? "text-purple-700":""}`}>{props.title}</p>
       {props.notificationCount>0 && props.notificationCount <= 10 &&
         <p className="px-2 py-1 text-white text-xs font-bold rounded-md bg-purple-700">{props.notificationCount}</p>
       }
--- src/components/nav/menu.js
+++ src/components/nav/menu.js
@@ -3,12 +3,12 @@ import MenuItem from "./menu-item"
 function Menu(props){
   return (
     <div className="mt-10">
-      <h3 className="text-lg font-semibold tracking-wide text-gray-500 mb-6 px-6">Menu</h3>
+      <h3 className="text-lg font-semibold tracking-wide text-gray-500 mb-6 px-6 cursor-pointer">Menu</h3>
       <MenuItem title="Tasks" notificationCount={0} active={true}/>
-      <MenuItem title="Inbox" notificationCount={12}/>
-      <MenuItem title="Projects" notificationCount={0}/>
-      <MenuItem title="Meetings"  notificationCount={2}/>
-      <MenuItem title="Settings"  notificationCount={0}/>
+      <MenuItem title="Inbox" notificationCount={12} active={false}/>
+      <MenuItem title="Projects" notificationCount={0} active={false}/>
+      <MenuItem title="Meetings"  notificationCount={2} active={false}/>
+      <MenuItem title="Settings"  notificationCount={0} active={false}/>
     </div>
   )
 }
